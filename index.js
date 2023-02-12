e275101b2b1271fcd2478be4e51a1d93eb51d5d0
Ashwin-krish-nan <ashwinraj786.as@gmail.com>
2023-02-13 03:31:08 +0530

    completed

--- index.js
+++ index.js
@@ -24,3 +24,77 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+     // Load the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // Create the player object once the API code has loaded.
+      var players = [];
+      function onYouTubeIframeAPIReady() {
+        createPlayer('cover-image-1', 'video-container-1', 'Hn3w0Zh0vQc');
+        createPlayer('cover-image-2', 'video-container-2', 'tQqq4Id6vM8');
+        
+        // Add calls to createPlayer() for each additional video as needed
+      }
+
+      // Create a new player instance for a video
+      function createPlayer(coverImageId, videoContainerId, videoId) {
+        var player = new YT.Player(videoContainerId, {
+          height: '360',
+          width: '640',
+          videoId: videoId,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+        players.push(player);
+          // Show the video when the user clicks on the cover image.
+        var coverImage = document.getElementById(coverImageId);
+        coverImage.addEventListener('click', function() {
+          coverImage.style.display = 'none';
+          document.getElementById(videoContainerId).style.display = 'block';
+          player.playVideo();
+        });
+      }
+
+      // The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // The API calls this function when the player's state changes.
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.ENDED) {
+          // Do something when the video has ended.
+        }
+      }
+
+
+
+/*       const accessToken = "YOUR_ACCESS_TOKEN";
+const userName = "Ashwin-krish-nan";
+
+fetch(`https://api.github.com/users/${userName}/events/public?access_token=${accessToken}`)
+  .then(response => response.json())
+  .then(events => {
+    const calendar = document.getElementById("calendar");
+
+    events.forEach(event => {
+      const eventDate = new Date(event.created_at).toLocaleDateString();
+      const eventType = event.type;
+
+      calendar.innerHTML += `
+        <p>
+          <strong>${eventDate}</strong>
+          <br>
+          ${eventType}
+        </p>
+      `;
+    });
+  }); */
